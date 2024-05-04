4686e6af35fffb3084fd9ce90697a2f8388e3110
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2024-05-05 01:14:52 +0700

    feat: init database 🚀

---
+++ db/migrations/20240504165054_create_table_user.up.sql
@@ -0,0 +1,60 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password VARCHAR(15) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TYPE cat_race AS ENUM (
+    'Persian',
+    'Maine Coon',
+    'Siamese',
+    'Ragdoll',
+    'Bengal',
+    'Sphynx',
+    'British Shorthair',
+    'Abyssinian',
+    'Scottish Fold',
+    'Birman'
+);
+
+CREATE TYPE cat_sex AS ENUM (
+    'male',
+    'female'
+);
+
+CREATE TABLE cats (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    has_matched BOOLEAN DEFAULT FALSE,
+    name VARCHAR(30) NOT NULL,
+    race cat_race NOT NULL,  
+    sex cat_sex NOT NULL, 
+    age_in_month INTEGER, 
+    description VARCHAR(200) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE cat_image (
+    id SERIAL PRIMARY KEY,
+    cat_id INTEGER,
+    image_url TEXT NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    FOREIGN KEY (cat_id) REFERENCES cats(id)
+);
+
+CREATE TABLE match_cats (
+    id SERIAL PRIMARY KEY,
+    match_cat_id INTEGER,
+    user_cat_id INTEGER,
+    message VARCHAR(120) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    FOREIGN KEY (match_cat_id) REFERENCES cats(id),
+    FOREIGN KEY (user_cat_id) REFERENCES cats(id)
+);
---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+	password = ""
+	dbname = "cats_social"
+)
+
+var (
+	db *sql.DB
+	err error
+)
+
+func main(){
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+}
+
+// type User struct {
+// 	ID int 
+// 	Name string
+// 	Email string
+// 	Password string
+// 	CreatedAt time.Time
+// 	UpdatedAt time.Time
+// }
+
+// func createUser(){
+// 	var user = User{}
+
+// 	sqlStatement := `
+// 	INSERT INTO users (name, email, password, createdat, updatedat)
+// 	VALUES ($1, $2, $3, $4, $5)
+// 	RETURNING *
+// 	`
+
+// 	err := db.QueryRow(sqlStatement, "Ismail", "ismailbinmail@gmail.com", "123456", time.Now(),time.Now()).
+// 	Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.CreatedAt, &user.UpdatedAt)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Printf("New user ID: %d\n", user.ID)
+// }
+
+// type Cat struct {
+// 	ID int 
+// 	UserId int
+// 	HasMatched bool
+// 	Name string
+// 	Race string
+// 	Sex string
+// 	AgeInMonth int
+// 	Description string
+// 	CreatedAt time.Time
+// 	UpdatedAt time.Time
+// }
+
+// func createCat(){
+// 	var cat = Cat{}
+
+// 	sqlStatement := `
+// 	INSERT INTO cats (user_id, hasmatched, name, race, sex, ageinmonth, description, createdat, updatedat)
+// 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+// 	RETURNING *
+// 	`
+
+// 	err := db.QueryRow(sqlStatement, 3, false, "Mely", "Persian", "female", 3, "Mely is a cute cat", time.Now(),time.Now()).
+// 	Scan(&cat.ID, &cat.UserId, &cat.HasMatched, &cat.Name, &cat.Race, &cat.Sex, &cat.AgeInMonth, &cat.Description, &cat.CreatedAt, &cat.UpdatedAt)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Printf("New cat ID: %d\n", cat.ID)
+// }
+
+// type CatImage struct {
+// 	ID int 
+// 	CatId int
+// 	ImageUrl string
+// 	CreatedAt time.Time
+// 	UpdatedAt time.Time
+// }
+
+// func createCatImage(){
+// 	var catImage = CatImage{}
+
+// 	sqlStatement := `
+// 	INSERT INTO catimage (cat_id, image_url, createdat, updatedat)
+// 	VALUES ($1, $2, $3, $4)
+// 	RETURNING *
+// 	`
+
+// 	err := db.QueryRow(sqlStatement, 1, "https://o-cdn-cas.sirclocdn.com/parenting/images/Cara-Merawat-Kucing-Persia.width-800.format-webp.webp", time.Now(),time.Now()).
+// 	Scan(&catImage.ID, &catImage.CatId, &catImage.ImageUrl, &catImage.CreatedAt, &catImage.UpdatedAt)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Printf("New cat image ID: %d\n", catImage.ID)
+// }
+
+// type MatchCats struct {
+// 	ID int
+// 	MatchCatID int
+// 	UserCatID int
+// 	Message string
+// 	CreatedAt time.Time
+// 	UpdatedAt time.Time
+// }
+
+// func createMatchCats(){
+// 	var matchCats = MatchCats{}
+
+// 	sqlStatement := `
+// 	INSERT INTO matchcats (matchcatid, usercatid, message, createdat, updatedat)
+// 	VALUES ($1, $2, $3, $4, $5)
+// 	RETURNING *
+// 	`
+
+// 	err := db.QueryRow(sqlStatement, 1, 2, "I love you Mely as a cat persia", time.Now(),time.Now()).
+// 	Scan(&matchCats.ID, &matchCats.MatchCatID, &matchCats.UserCatID, &matchCats.Message, &matchCats.CreatedAt, &matchCats.UpdatedAt)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Printf("New match cat ID: %d\n", matchCats.ID)
+// }
